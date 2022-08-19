c9f006c34314c0197b23d3ba0d76f60cf9b3a396
M-Rashford <hax.m.rashford@gmail.com>
2022-08-19 14:16:38 -0400

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "messageCreate",
     async execute(message, client) {
-        if (message.author.bot && !["1009875024217382993"].includes(message.channel.id)) return;
+        if (["1009875024217382993"].includes(message.channel.id)) return;
         setTimeout(async () => {
             try {
                 await message.delete();
