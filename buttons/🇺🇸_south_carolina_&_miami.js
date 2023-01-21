5fc961a3595fa316dc8ae20952540e2a02fd701b
M-Rashford <hax.m.rashford@gmail.com>
2023-01-21 14:41:18 -0500

    edit with miami included

--- buttons/🇺🇸_south_carolina_&_miami.js
+++ buttons/🇺🇸_south_carolina_&_miami.js
@@ -3,14 +3,14 @@ const flag = __filename.slice(__dirname.length + 1, -(name.length + 4));
 module.exports = {
     name: name,
     async execute(interaction, client) {
-        const rooms = client.cachedRooms.cache.sort((a, b) => a.roomId - b.roomId).filter((room) => room.region.toLowerCase() == "south carolina");
+        const rooms = client.cachedRooms.cache.sort((a, b) => a.roomId - b.roomId).filter((room) => room.region.toLowerCase() == "south carolina & miami");
         const roomLinks = rooms.map((room) => `${flag} ${room.roomId}. ${room.link}`).join("\n");
         const embed = {
             color: 0x049074,
             title: "Global Hosting Services",
             url: "https://discord.gg/ksZY2E6Wpz",
             description: `List of rooms for selected location`,
-            fields: [{ name: "\u200b", value: `${flag} **__South Carolina Rooms__** ${flag}\n\n${roomLinks}` }],
+            fields: [{ name: "\u200b", value: `${flag} **__South Carolina & Miami Rooms__** ${flag}\n\n${roomLinks}` }],
             thumbnail: {
                 url: "https://cdn.discordapp.com/attachments/933693278379978762/1007613680566927480/78E6812D-47E5-4FDA-BEC8-68527FE1FA3A.jpg",
             },
